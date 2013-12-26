da9e44e3e551e0cdd1d780f7e26ec9d88fba23fd
Eli Fatsi <eli.fatsi@viget.com>
2013-12-26 09:07:48 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaundryAlert::Application.config.secret_key_base = 'f5e7d6c2a378dc06af8ac9fd4856b702a77107ca05383b09dd5d1eda841db6ee2d2c7770c12be05dbfa98853c69eb2720ee38a9659d9733ff386c4952bafcc28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaundryAlert::Application.config.session_store :cookie_store, key: '_laundry_alert_session'
